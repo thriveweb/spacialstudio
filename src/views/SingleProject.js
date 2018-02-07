8ab695c677925f997258088bcd622388cf263f96
Alex <alex@thriveweb.com.au>
2018-02-07 17:30:44 +1000

    fix gallery

--- src/views/SingleProject.js
+++ src/views/SingleProject.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-import PageHeader from '../components/PageHeader'
+import Content from '../components/Content'
+import Gallery from '../components/Gallery'
 
 import './SingleProject.css'
 
@@ -10,18 +11,43 @@ export default ({ singleProject }) => (
     <Helmet>
       <title>{singleProject.title}</title>
     </Helmet>
-    <PageHeader title={singleProject.title} />
+
     <div className='section thin'>
-      <div className='container'>
-        {singleProject.projectFeaturedImage}
-        {/* {singleProject.projectGalleryImages} */}
-        {console.log(singleProject.content)}goes here
-        {singleProject.title}
-        {singleProject.content}
-        {singleProject.year}
-        --
-        {singleProject.testimonial}
-        {singleProject.role}
+      <div className='container Flex alignStart justifyBetween'>
+        <div className='SingleProject--gallery'>
+          <Gallery
+            images={singleProject.projectGalleryImages.map(
+              obj => obj.galleryimage
+            )}
+          />
+        </div>
+        <div className='SingleProject--content'>
+          <h1>{singleProject.title}</h1>
+          <Content source={singleProject.content} />
+          <div className='breakout '>
+            <div className='year breakout--box'>
+              <h3>Year</h3>
+              <p>{singleProject.year}</p>
+            </div>
+            <div className='type breakout--box'>
+              <h3>Type of project</h3>
+              <p>{singleProject.typeofproject}</p>
+            </div>
+            <div className='client breakout--box'>
+              <h3>Client</h3>
+              <p>{singleProject.client}</p>
+            </div>
+          </div>
+          <div className='testimonials'>
+            <blockquote>{singleProject.testimonial}</blockquote>
+            <div className='from Flex alignStart justifyStart'>
+              <p>{singleProject.name}</p>
+              <p>
+                <small>{singleProject.role}</small>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
