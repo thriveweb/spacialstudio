0324b42cfb5cb0b2d7f858ab54d9804f1160e609
Alex <alex@thriveweb.com.au>
2018-02-21 16:58:16 +1000

    wrap password over client area

--- src/views/ClientArea.js
+++ src/views/ClientArea.js
@@ -2,33 +2,36 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import PageHeader from '../components/PageHeader'
+import PasswordProtected from '../components/PasswordProtected.js'
 import Content from '../components/Content.js'
 import Accordion from '../components/Accordion.js'
 import './ClientArea.css'
 
 export default ({ page }) => (
-  <main className='ClientArea'>
-    <Helmet>
-      <title>{page.title}</title>
-    </Helmet>
+  <PasswordProtected password={page.clientAreaPassword}>
+    <main className='ClientArea'>
+      <Helmet>
+        <title>{page.title}</title>
+      </Helmet>
 
-    <PageHeader title={page.title} />
+      <PageHeader title={page.title} />
 
-    {page.contentSection && (
-      <section className='section thin'>
-        <div className='container'>
-          <h1 className='taCenter'>{page.title}</h1>
-          <Content source={page.contentSection} className='threeColumn' />
-        </div>
-      </section>
-    )}
+      {page.contentSection && (
+        <section className='section thin'>
+          <div className='container'>
+            <h1 className='taCenter'>{page.title}</h1>
+            <Content source={page.contentSection} className='threeColumn' />
+          </div>
+        </section>
+      )}
 
-    {page.accordion && (
-      <section className='section thin'>
-        <div className='container'>
-          <Accordion items={page.accordion} />
-        </div>
-      </section>
-    )}
-  </main>
+      {page.accordion && (
+        <section className='section thin'>
+          <div className='container'>
+            <Accordion items={page.accordion} />
+          </div>
+        </section>
+      )}
+    </main>
+  </PasswordProtected>
 )
